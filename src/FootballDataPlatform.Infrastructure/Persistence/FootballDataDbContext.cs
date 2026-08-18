05126974f4496c8dbe2219c9b849ed77a251b9f3
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-18 12:35:02 +0100

    feat: add Competition to DbContext

--- src/FootballDataPlatform.Infrastructure/Persistence/FootballDataDbContext.cs
+++ src/FootballDataPlatform.Infrastructure/Persistence/FootballDataDbContext.cs
@@ -1,16 +1,16 @@
+using FootballDataPlatform.Domain.Competitions;
 using FootballDataPlatform.Domain.Teams;
 using Microsoft.EntityFrameworkCore;
+
 namespace FootballDataPlatform.Infrastructure.Persistence;
 
-public class FootballDataDbContext(DbContextOptions<FootballDataDbContext> options)
-    : DbContext(options)
+public class FootballDataDbContext(DbContextOptions<FootballDataDbContext> options) : DbContext(options)
 {
-   
     public DbSet<Team> Teams => Set<Team>();
+    public DbSet<Competition> Competitions => Set<Competition>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(
-            typeof(FootballDataDbContext).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FootballDataDbContext).Assembly);
     }
 }
-
