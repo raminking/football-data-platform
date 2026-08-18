53eafae5e8ecaafb56cfe7c049c686a61e9ce891
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-18 12:35:09 +0100

    feat: register Competition repository

--- src/FootballDataPlatform.Infrastructure/DependencyInjection.cs
+++ src/FootballDataPlatform.Infrastructure/DependencyInjection.cs
@@ -1,23 +1,22 @@
 using FootballDataPlatform.Application.Abstractions.Persistence;
+using FootballDataPlatform.Infrastructure.Persistence;
+using FootballDataPlatform.Infrastructure.Persistence.Competitions;
+using FootballDataPlatform.Infrastructure.Persistence.Teams;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using FootballDataPlatform.Infrastructure.Persistence;
-using FootballDataPlatform.Infrastructure.Persistence.Teams;
 
 namespace FootballDataPlatform.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(
-        this IServiceCollection services,
-        IConfiguration configuration)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<FootballDataDbContext>(options =>
-            options.UseNpgsql(
-                configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
 
         services.AddScoped<ITeamRepository, TeamRepository>();
+        services.AddScoped<ICompetitionRepository, CompetitionRepository>();
         return services;
     }
 }
