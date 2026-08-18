cbff29e418ce668e6c3509c9bb80349006e8a050
Ramin <Ramin.88@outlook.com>
2026-08-18 16:04:17 +0100

    feat: add Match persistence

---
+++ src/FootballDataPlatform.Infrastructure/Migrations/20260818143137_AddMatches.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballDataPlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMatches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
