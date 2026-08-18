c61417cb98c121f3b703e54ba51f7b800a832257
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-18 12:35:48 +0100

    test: add Competition domain coverage

---
+++ tests/FootballDataPlatform.Tests/Competitions/Domain/CompetitionTests.cs
@@ -0,0 +1,45 @@
+using FootballDataPlatform.Domain.Competitions;
+
+namespace FootballDataPlatform.Tests.Competitions.Domain;
+
+public class CompetitionTests
+{
+    [Fact]
+    public void Constructor_WithValidData_CreatesCompetition()
+    {
+        var competition = new Competition("Premier League", "England", "epl");
+
+        Assert.NotEqual(Guid.Empty, competition.Id);
+        Assert.Equal("Premier League", competition.Name);
+        Assert.Equal("England", competition.Country);
+        Assert.Equal("EPL", competition.Code);
+    }
+
+    [Fact]
+    public void Constructor_WithEmptyName_Throws()
+    {
+        Assert.Throws<ArgumentException>(() => new Competition("", "England", "EPL"));
+    }
+
+    [Fact]
+    public void Constructor_WithEmptyCountry_Throws()
+    {
+        Assert.Throws<ArgumentException>(() => new Competition("Premier League", "", "EPL"));
+    }
+
+    [Fact]
+    public void Constructor_WithEmptyCode_Throws()
+    {
+        Assert.Throws<ArgumentException>(() => new Competition("Premier League", "England", ""));
+    }
+
+    [Fact]
+    public void UpdateDetails_NormalizesCode()
+    {
+        var competition = new Competition("Premier League", "England", "EPL");
+
+        competition.UpdateDetails("Premier League", "England", " pl ");
+
+        Assert.Equal("PL", competition.Code);
+    }
+}
