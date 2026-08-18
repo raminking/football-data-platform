2908f58ee4140108685f3ab4b3e8d91d18317ac5
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-18 12:34:15 +0100

    feat: configure Competition persistence

---
+++ src/FootballDataPlatform.Infrastructure/Persistence/Configurations/CompetitionConfiguration.cs
@@ -0,0 +1,17 @@
+using FootballDataPlatform.Domain.Competitions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FootballDataPlatform.Infrastructure.Persistence.Configurations;
+
+public class CompetitionConfiguration : IEntityTypeConfiguration<Competition>
+{
+    public void Configure(EntityTypeBuilder<Competition> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(150).IsRequired();
+        builder.Property(x => x.Country).HasMaxLength(100).IsRequired();
+        builder.Property(x => x.Code).HasMaxLength(20).IsRequired();
+        builder.HasIndex(x => new { x.Name, x.Country, x.Code }).IsUnique();
+    }
+}
