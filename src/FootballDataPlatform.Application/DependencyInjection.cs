6aa3becb553fd3d010a4ebd3487081c0bb5d2034
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-19 17:12:50 +0100

    feat: register competition import service

--- src/FootballDataPlatform.Application/DependencyInjection.cs
+++ src/FootballDataPlatform.Application/DependencyInjection.cs
@@ -15,6 +15,7 @@ public static class DependencyInjection
                 typeof(DependencyInjection).Assembly));
 
         services.AddScoped<ITeamImportService, TeamImportService>();
+        services.AddScoped<ICompetitionImportService, CompetitionImportService>();
 
         return services;
     }
