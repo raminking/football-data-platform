507f456142e434dcdc61cabefcc9758472a14af6
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-19 15:08:19 +0100

    feat: register team import service

--- src/FootballDataPlatform.Application/DependencyInjection.cs
+++ src/FootballDataPlatform.Application/DependencyInjection.cs
@@ -1,3 +1,6 @@
+using FootballDataPlatform.Application.Abstractions.ExternalData;
+using FootballDataPlatform.Application.Abstractions.Persistence;
+using FootballDataPlatform.Application.ExternalData;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FootballDataPlatform.Application;
@@ -10,6 +13,8 @@ public static class DependencyInjection
         services.AddMediatR(config =>
             config.RegisterServicesFromAssembly(
                 typeof(DependencyInjection).Assembly));
+
+        services.AddScoped<ITeamImportService, TeamImportService>();
 
         return services;
     }
