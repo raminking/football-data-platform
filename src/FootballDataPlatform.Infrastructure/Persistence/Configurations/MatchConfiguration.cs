7a8d8817f515a36fff788469b03fdb68d0cfa09c
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-20 21:17:25 +0100

    refactor: configure bigint match foreign keys and public uuid

--- src/FootballDataPlatform.Infrastructure/Persistence/Configurations/MatchConfiguration.cs
+++ src/FootballDataPlatform.Infrastructure/Persistence/Configurations/MatchConfiguration.cs
@@ -11,35 +11,16 @@ public sealed class MatchConfiguration : IEntityTypeConfiguration<Domain.Match.M
     public void Configure(EntityTypeBuilder<Domain.Match.Match> builder)
     {
         builder.HasKey(x => x.Id);
-
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        builder.Property(x => x.PublicId).IsRequired();
+        builder.HasIndex(x => x.PublicId).IsUnique();
         builder.Property(x => x.ScheduledAt).IsRequired();
         builder.Property(x => x.Stage).HasConversion<string>().HasMaxLength(30).IsRequired();
         builder.Property(x => x.Status).HasConversion<string>().HasMaxLength(20).IsRequired();
         builder.Property(x => x.Result).HasConversion<string>().HasMaxLength(20);
-
-        builder.Property(x => x.HomeScore);
-        builder.Property(x => x.AwayScore);
-        builder.Property(x => x.HalfTimeHomeScore);
-        builder.Property(x => x.HalfTimeAwayScore);
-
-        builder.HasOne<Season>()
-            .WithMany()
-            .HasForeignKey(x => x.SeasonId)
-            .OnDelete(DeleteBehavior.Restrict)
-            .IsRequired();
-
-        builder.HasOne<Team>()
-            .WithMany()
-            .HasForeignKey(x => x.HomeTeamId)
-            .OnDelete(DeleteBehavior.Restrict)
-            .IsRequired();
-
-        builder.HasOne<Team>()
-            .WithMany()
-            .HasForeignKey(x => x.AwayTeamId)
-            .OnDelete(DeleteBehavior.Restrict)
-            .IsRequired();
-
+        builder.HasOne<Season>().WithMany().HasForeignKey(x => x.SeasonId).OnDelete(DeleteBehavior.Restrict).IsRequired();
+        builder.HasOne<Team>().WithMany().HasForeignKey(x => x.HomeTeamId).OnDelete(DeleteBehavior.Restrict).IsRequired();
+        builder.HasOne<Team>().WithMany().HasForeignKey(x => x.AwayTeamId).OnDelete(DeleteBehavior.Restrict).IsRequired();
         builder.HasIndex(x => new { x.SeasonId, x.ScheduledAt });
         builder.HasIndex(x => x.HomeTeamId);
         builder.HasIndex(x => x.AwayTeamId);
