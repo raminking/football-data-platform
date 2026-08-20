8c9925feda25a9c87b11438b1ef02224eb4efb56
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-20 23:45:46 +0100

    test: align competition domain tests with internal id and public uuid

--- tests/FootballDataPlatform.Tests/Competitions/Domain/CompetitionTests.cs
+++ tests/FootballDataPlatform.Tests/Competitions/Domain/CompetitionTests.cs
@@ -9,37 +9,25 @@ public class CompetitionTests
     {
         var competition = new Competition("Premier League", "England", "epl");
 
-        Assert.NotEqual(Guid.Empty, competition.Id);
+        Assert.Equal(0, competition.Id);
+        Assert.NotEqual(Guid.Empty, competition.PublicId);
         Assert.Equal("Premier League", competition.Name);
         Assert.Equal("England", competition.Country);
         Assert.Equal("EPL", competition.Code);
     }
 
     [Fact]
-    public void Constructor_WithEmptyName_Throws()
-    {
-        Assert.Throws<ArgumentException>(() => new Competition("", "England", "EPL"));
-    }
-
+    public void Constructor_WithEmptyName_Throws() => Assert.Throws<ArgumentException>(() => new Competition("", "England", "EPL"));
     [Fact]
-    public void Constructor_WithEmptyCountry_Throws()
-    {
-        Assert.Throws<ArgumentException>(() => new Competition("Premier League", "", "EPL"));
-    }
-
+    public void Constructor_WithEmptyCountry_Throws() => Assert.Throws<ArgumentException>(() => new Competition("Premier League", "", "EPL"));
     [Fact]
-    public void Constructor_WithEmptyCode_Throws()
-    {
-        Assert.Throws<ArgumentException>(() => new Competition("Premier League", "England", ""));
-    }
+    public void Constructor_WithEmptyCode_Throws() => Assert.Throws<ArgumentException>(() => new Competition("Premier League", "England", ""));
 
     [Fact]
     public void UpdateDetails_NormalizesCode()
     {
         var competition = new Competition("Premier League", "England", "EPL");
-
         competition.UpdateDetails("Premier League", "England", " pl ");
-
         Assert.Equal("PL", competition.Code);
     }
 }
