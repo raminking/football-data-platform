504a50d41e0b9e7ee0181a9cf88c90ca30565280
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-20 21:17:15 +0100

    refactor: configure internal competition identity and public uuid

--- src/FootballDataPlatform.Infrastructure/Persistence/Configurations/CompetitionConfiguration.cs
+++ src/FootballDataPlatform.Infrastructure/Persistence/Configurations/CompetitionConfiguration.cs
@@ -9,6 +9,9 @@ public class CompetitionConfiguration : IEntityTypeConfiguration<Competition>
     public void Configure(EntityTypeBuilder<Competition> builder)
     {
         builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        builder.Property(x => x.PublicId).IsRequired();
+        builder.HasIndex(x => x.PublicId).IsUnique();
         builder.Property(x => x.Name).HasMaxLength(150).IsRequired();
         builder.Property(x => x.Country).HasMaxLength(100).IsRequired();
         builder.Property(x => x.Code).HasMaxLength(20).IsRequired();
