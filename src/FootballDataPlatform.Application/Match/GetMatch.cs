b2f962b38e22f431233a07326f113866bc6a7896
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-20 21:19:32 +0100

    refactor: get matches by public uuid

--- src/FootballDataPlatform.Application/Match/GetMatch.cs
+++ src/FootballDataPlatform.Application/Match/GetMatch.cs
@@ -1,20 +1,15 @@
 using FootballDataPlatform.Application.Abstractions.Persistence;
 using FootballDataPlatform.Application.Common;
-using FootballDataPlatform.Domain.Match;
 using MediatR;
 
 namespace FootballDataPlatform.Application.Match;
 
-public record GetMatchQuery(Guid Id) : IRequest<Result<Domain.Match.Match>>;
-
-internal sealed class GetMatchHandler(IMatchRepository repository)
-    : IRequestHandler<GetMatchQuery, Result<Domain.Match.Match>>
+public record GetMatchQuery(Guid PublicId) : IRequest<Result<Domain.Match.Match>>;
+internal sealed class GetMatchHandler(IMatchRepository repository) : IRequestHandler<GetMatchQuery, Result<Domain.Match.Match>>
 {
     public async Task<Result<Domain.Match.Match>> Handle(GetMatchQuery query, CancellationToken ct)
     {
-        var match = await repository.GetByIdAsync(query.Id, ct);
-        return match is null
-            ? Result<Domain.Match.Match>.Failure("Match not found.")
-            : Result<Domain.Match.Match>.Success(match);
+        var match = await repository.GetByPublicIdAsync(query.PublicId, ct);
+        return match is null ? Result<Domain.Match.Match>.Failure("Match not found.") : Result<Domain.Match.Match>.Success(match);
     }
 }
