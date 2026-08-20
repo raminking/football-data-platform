d7262a42ea0374cf25ea98dcc2bd7ac353173390
Ramin <32914229+raminking@users.noreply.github.com>
2026-08-20 21:17:12 +0100

    refactor: configure internal team identity and public uuid

--- src/FootballDataPlatform.Infrastructure/Persistence/Configurations/TeamConfiguration.cs
+++ src/FootballDataPlatform.Infrastructure/Persistence/Configurations/TeamConfiguration.cs
@@ -9,15 +9,11 @@ public class TeamConfiguration : IEntityTypeConfiguration<Team>
     public void Configure(EntityTypeBuilder<Team> builder)
     {
         builder.HasKey(team => team.Id);
-
-        builder.Property(team => team.LogoUrl)
-            .HasMaxLength(2048);
-
-        builder.Property(team => team.OfficialWebsiteUrl)
-            .HasMaxLength(2048);
-
-        builder
-            .HasIndex(team => new { team.Name, team.Country })
-            .IsUnique();
+        builder.Property(team => team.Id).ValueGeneratedOnAdd();
+        builder.Property(team => team.PublicId).IsRequired();
+        builder.HasIndex(team => team.PublicId).IsUnique();
+        builder.Property(team => team.LogoUrl).HasMaxLength(2048);
+        builder.Property(team => team.OfficialWebsiteUrl).HasMaxLength(2048);
+        builder.HasIndex(team => new { team.Name, team.Country }).IsUnique();
     }
 }
